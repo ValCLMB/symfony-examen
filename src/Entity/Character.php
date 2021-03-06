fa37b4f11513b0b86c83e60974a7f6f6b625f5a6
Rémi Jarjat <remi@dreeckan.net>
2021-03-06 21:29:01 +0100

    add base entity

---
+++ src/Entity/Character.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharacterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CharacterRepository::class)
+ */
+class Character
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $strength;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $healthPoint;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $defense;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStrength(): ?int
+    {
+        return $this->strength;
+    }
+
+    public function setStrength(int $strength): self
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    public function getHealthPoint(): ?int
+    {
+        return $this->healthPoint;
+    }
+
+    public function setHealthPoint(int $healthPoint): self
+    {
+        $this->healthPoint = $healthPoint;
+
+        return $this;
+    }
+
+    public function getDefense(): ?int
+    {
+        return $this->defense;
+    }
+
+    public function setDefense(int $defense): self
+    {
+        $this->defense = $defense;
+
+        return $this;
+    }
+}
